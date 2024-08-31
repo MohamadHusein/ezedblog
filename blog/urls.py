abdb50ceb081ec9c74f19efa4c7b72522086898b
cybersir <mohamd.qorbani383@gmail.com>
2024-08-31 22:14:56 +0330

    search_articles

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('detail/<slug:slug>'  , views.article_detail , name='article_detail'),
     path('list' , views.articles_list , name='articles_list'),
     path('category/<int:pk>',views.category_detail,  name='category_detail'),
+    path('search' , views.search , name='search_articles'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,13 @@ def category_detail(request , pk=None):
 
 
 
-
+def search(request):
+    q = request.GET.get('q')
+    articles = Article.objects.filter(title__icontains=q)
+    page_number = request.GET.get('page')
+    paginator = Paginator(articles, 1)
+    objects_list = paginator.get_page(page_number)
+    return render(request , 'blog/articles_list.html' , {'articles':objects_list})
 
 
 
